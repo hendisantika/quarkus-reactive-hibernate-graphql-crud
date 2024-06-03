950a6013daaac697b6b2866231bdc6c680054b71
hendisantika <hendisantika@yahoo.co.id>
2024-06-04 06:25:10 +0700

    Add ActorResource🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/controller/ActorResource.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.controller;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-reactive-hibernate-graphql-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/4/24
+ * Time: 06:24
+ * To change this template use File | Settings | File Templates.
+ */
+@GraphQLApi
+public class ActorResource {
+}
